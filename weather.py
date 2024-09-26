49f80548be63a06b7a8110e5f20789e312fe41ec
mohammedoh87 <aamina.oh87@gmail.com>
2024-09-26 15:51:08 +0300

    Functions

---
+++ weather.py
@@ -0,0 +1,8 @@
+def weather_condition():
+    print("The weather in spring is ", spring)
+    print("The weather in autumn is ", autumn)
+
+spring = "Rainy and cool"
+autumn = "Cold and dry"
+
+weather_condition()
